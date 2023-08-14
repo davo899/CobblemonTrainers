92a632dab59d91355f8c678496c5fc4a80356b0e
David Sadler <djs21@ic.ac.uk>
2023-08-14 14:53:07 +0100

    Added setting icons to pokemon view screen

--- common/src/main/java/com/selfdot/cobblemontrainers/screen/TrainerPokemonScreen.java
+++ common/src/main/java/com/selfdot/cobblemontrainers/screen/TrainerPokemonScreen.java
@@ -1,13 +1,22 @@
 package com.selfdot.cobblemontrainers.screen;
 
+import com.cobblemon.mod.common.CobblemonItems;
 import com.selfdot.cobblemontrainers.trainer.Trainer;
 import com.selfdot.cobblemontrainers.trainer.TrainerPokemon;
 import com.selfdot.cobblemontrainers.util.PokemonUtility;
 import net.minecraft.inventory.Inventory;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.text.Text;
 
 public class TrainerPokemonScreen extends Screen {
 
     private final TrainerPokemon trainerPokemon;
+    private int movesSlot;
+    private int abilitiesSlot;
+    private int evsSlot;
+    private int ivsSlot;
+    private int deleteSlot;
 
     public TrainerPokemonScreen(Trainer trainer, TrainerPokemon trainerPokemon) {
         super(new TrainerTeamScreen(trainer));
@@ -17,6 +26,32 @@ public class TrainerPokemonScreen extends Screen {
     @Override
     public void initialize(Inventory inventory) {
         inventory.setStack(columns / 2, PokemonUtility.pokemonToInfoItem(trainerPokemon.toPokemon()));
+
+        movesSlot = (columns * 2) + (columns / 2) - 2;
+        abilitiesSlot = movesSlot + 1;
+        evsSlot = abilitiesSlot + 1;
+        ivsSlot = evsSlot + 1;
+        deleteSlot = ivsSlot + 1;
+
+        ItemStack movesItem = new ItemStack(Items.MUSIC_DISC_5);
+        movesItem.setCustomName(Text.literal("Moves"));
+        inventory.setStack(movesSlot, movesItem);
+
+        ItemStack abilitiesItem = new ItemStack(CobblemonItems.CLOVER_SWEET.get());
+        abilitiesItem.setCustomName(Text.literal("Abilities"));
+        inventory.setStack(abilitiesSlot, abilitiesItem);
+
+        ItemStack evsItem = new ItemStack(CobblemonItems.FLOWER_SWEET.get());
+        evsItem.setCustomName(Text.literal("EVs"));
+        inventory.setStack(evsSlot, evsItem);
+
+        ItemStack ivsItem = new ItemStack(CobblemonItems.STAR_SWEET.get());
+        ivsItem.setCustomName(Text.literal("IVs"));
+        inventory.setStack(ivsSlot, ivsItem);
+
+        ItemStack deleteItem = new ItemStack(Items.BARRIER);
+        deleteItem.setCustomName(Text.literal("Delete Pokémon"));
+        inventory.setStack(deleteSlot, deleteItem);
     }
 
     @Override
